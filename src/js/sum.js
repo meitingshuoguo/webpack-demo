97eb2713a0966085921be4784eb67f158d56c91d
meitingshuoguo <hefkang@gmail.com>
2022-06-17 10:26:31 +0800

    init and a simple example for don't usewebpack

---
+++ src/js/sum.js
@@ -0,0 +1,3 @@
+export default function sum(...arg) {
+  return arg.reduce((p, c) => p + c, 0);
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import count from "./js/count.js";
+import sum from "./js/sum.js";
+
+console.log(count(10, 5));
+console.log(sum(10, 5));
