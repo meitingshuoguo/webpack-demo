e3ef1e27973a5ccd5c2ee92f9c4f1c9d73da2dbf
meitingshuoguo <hefkang@gmail.com>
2022-07-01 18:23:36 +0800

    sass/scss config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./css/index.css";
 import count from "./js/count.js";
 import sum from "./js/sum.js";
 import "./less/index.less";
+import "./scss/index.scss";
 
 console.log(count(10, 5));
 console.log(sum(10, 5));
