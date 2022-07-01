7fa0da775e562da4406d8322616fa319b6471784
meitingshuoguo <hefkang@gmail.com>
2022-07-01 18:31:36 +0800

    stylus config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import count from "./js/count.js";
 import sum from "./js/sum.js";
 import "./less/index.less";
 import "./scss/index.scss";
+import "./stylus/index.styl";
 
 console.log(count(10, 5));
 console.log(sum(10, 5));
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"],
+      },
     ],
   },
   // 插件
