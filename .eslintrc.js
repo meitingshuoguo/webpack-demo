50cd26b0ee6a0ee66c59682e2ee93dbd577a4228
meitingshuoguo <hefkang@gmail.com>
2022-07-04 10:31:38 +0800

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 解析选项
+  parserOptions: {
+    ecmaVersion: 6, //ES语法版本
+    sourceType: "module", //ES模块化
+    ecmaFeatures: {
+      jsx: true, //如果是React项目，就需要开启jsx语法
+    },
+  },
+  // 可以继承某一种风格的规则
+  extends: ["eslint:recommended"],
+  env: {
+    node: true, //启用node中全局变量
+    browser: true, //启用浏览器中全局变量
+  },
+  // 具体的检查规则
+  rules: {
+    "no-var": 2,
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path"); //node.js的核心模块，专门用来处理路径问题
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
   // 入口
@@ -63,7 +64,12 @@ module.exports = {
     ],
   },
   // 插件
-  plugins: [],
+  plugins: [
+    new ESLintPlugin({
+      // 检测哪些文件
+      context: path.resolve(__dirname, "src"),
+    }),
+  ],
   // 模式
   mode: "development",
 };
