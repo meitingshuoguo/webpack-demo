81ac6d918a74774e9f8816a8f61071ef63d4d69c
meitingshuoguo <hefkang@gmail.com>
2022-07-13 11:05:01 +0800

    tree shaking test(prod mode default is on)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import "./css/iconfont.css";
 import "./css/index.css";
 import count from "./js/count.js";
+import { mul } from "./js/math";
 import sum from "./js/sum.js";
 import "./less/index.less";
 import "./scss/index.scss";
 import "./stylus/index.styl";
 
+console.log(mul(5, 6));
 console.log(count(10, 50));
 console.log(sum(10, 51));
 
